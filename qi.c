4e5d30b4e59f0d3c197f58209a901a945a6478ca
CodingNoob <CodingNoob1224@gmail.com>
2023-02-25 22:03:02 +0800

    qi_one

---
+++ qi.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+void OneLine(int);
+void triangle(int, int);
+
+void triangle(int input, int times){
+    if(input < times){
+        //printf("end");
+        return;
+    }else{
+        //printf("Line %d: ", times);
+        OneLine(times);
+    }triangle(input, times+1);
+}
+void OneLine(int star){
+    if(star == 0){
+        printf("\n");
+        return;
+    }
+    printf("*");
+    OneLine(star-1);
+}
+
+
+int main(){
+    int x;
+    while(scanf("%d",&x)!=EOF){
+        triangle(x, 1);
+    }
+    return 0;
+}
